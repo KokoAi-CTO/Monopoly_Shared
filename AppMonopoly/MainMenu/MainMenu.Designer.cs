4d888f41ccc7b8f8e1837c2030ad10cbb64114c9
Sneha Dcunha <sneha.dcunha1908@gmail.com>
2019-01-15 22:50:47 -0500

    i did main menu- sd

--- AppMonopoly/MainMenu/MainMenu.Designer.cs
+++ AppMonopoly/MainMenu/MainMenu.Designer.cs
@@ -132,17 +132,18 @@
             // aboutToolStripMenuItem
             // 
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(146, 30);
             this.aboutToolStripMenuItem.Text = "About";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(699, 82);
+            this.pictureBox1.Location = new System.Drawing.Point(667, 82);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(223, 219);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // lblMonopoly
             // 
--- AppMonopoly/MainMenu/MainMenu.cs
+++ AppMonopoly/MainMenu/MainMenu.cs
@@ -21,5 +21,10 @@ namespace MainMenu
         {
 
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
